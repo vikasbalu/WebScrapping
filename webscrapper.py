60c61771b23f6778beb92102d902d15e70e2d791
vikasbalu <vikasbalu012@gmail.com>
2023-12-31 01:11:46 -0500

    The webscrapper will scrape the github wesite and copy into a output file

--- webscrapper.py
+++ webscrapper.py
@@ -6,20 +6,25 @@ url = 'https://github.com/'+github_user #from which url we are scarpping
 
 r = requests.get(url) # send request to that url 
 
-soup = bs(r.content, 'html.parser') # soup is used to scrap the website
-#print(soup.prettify()) # this will print whole html content of the webpage in a prettified format
+if r.status_code == 200:
 
-# Open a file for writing ('w' stands for write)
-with open('output.txt', 'w') as file:
-    # Write content to the file
-    file.write(soup.prettify())
+    soup = bs(r.content, 'html.parser') # soup is used to scrap the website
+   
+    #print(soup.prettify()) # this will print whole html content of the webpage in a prettified format
+   
+    # Open a file for writing ('w' stands for write)
+    with open('output.txt', 'w', encoding='utf-8') as file:
+        # Write content to the file
+        file.write(soup.prettify())
 
-print('Content has been written to the file.')
+    print('Content has been written to the file.')
 
+    profile_image = soup.find('img')['src'] # used to retrive the profile pic of the github profile  
 
-profile_image = soup.find('img')['src']
+    print(profile_image) # print the link 
+else:
 
-#print(profile_image)
+    print(" Failed to retrive the website")
 
 
 
